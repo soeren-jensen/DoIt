da69a75d383474e84a75bfefe67b2a726b8a2252
Søren Munkholm Jensen <sorenmunkholmjensen@srens-macbook-pro.local>
2016-08-28 18:31:34 +0200

    Initial Commit

---
+++ DoIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DoIt
+//
+//  Created by Søren Munkholm Jensen on 28/08/2016.
+//  Copyright © 2016 smj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
